3aed78e80f1a8b44a4a85d5b0b735418982acb7a
chithrav <chithra211@gmail.com>
2025-09-25 00:33:37 -0400

    Add files via upload
    Learn Variables

---
+++ PW_SEP/Day1/variables.js
@@ -0,0 +1,23 @@
+//one line comment
+
+/* this is for
+commenting a paragraph 
+alt+shift+a */
+
+console.log("Welcome to Playwrite")
+
+let firstName = "chithra";
+let companyName = "HCL";
+let mobileNumber = 1234567890;
+let isAutomation = true;
+let hasPlaywright ;
+console.log(firstName);
+console.log(typeof firstName);
+console.log(companyName);
+console.log(typeof companyName);
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+console.log(isAutomation);
+console.log(typeof isAutomation);
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
